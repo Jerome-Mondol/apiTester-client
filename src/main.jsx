16f1071429da7fc5a231fd26e0d0b2f982eab153
Jerome Mondol <jeromemondol11@gmail.com>
2025-11-20 16:10:33 +0600

    init: router init

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { Theme } from '@radix-ui/themes'
+import { RouterProvider } from 'react-router/dom'
+import { router } from './router/router.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Theme>
-      <App />
+      <RouterProvider router={router} />
     </Theme>
   </StrictMode>,
 )
